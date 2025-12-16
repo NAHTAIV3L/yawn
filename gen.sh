4ed483785fa8914cce3caa42e7f0508d46752451
Riley Beckett <rbeckettvt@gmail.com>
2025-12-15 19:55:22 -0500

    make gen.sh even better

--- gen.sh
+++ gen.sh
@@ -2,7 +2,21 @@
 
 bin="yawn"
 libs=""
+release=0
 builddir="objs"
+
+while [ "$1" ]; do
+	case "$1" in
+        --release|-r) release=1 ;;
+        --builddir*) builddir="$(echo $1 | cut -d= -f2)" ;;
+		-*) exit 1 ;;
+	esac
+	shift
+done
+
+CFLAGS=${CFLAGS:-""}
+
+[ $release -eq 1 ] && CFLAGS="$CFLAGS -O2" || CFLAGS="$CFLAGS -Wall -g"
 
 write() {
     echo "$@" >> build.ninja
@@ -10,7 +24,9 @@ write() {
 
 rm -f build.ninja
 
-write "cflags = -Wall -g $(pkg-config --cflags $libs 2>/dev/null)"
+
+write "builddir = $builddir"
+write "cflags = $CFLAGS $(pkg-config --cflags $libs 2>/dev/null)"
 write "libs = $(pkg-config --libs $libs 2>/dev/null)"
 
 write 'rule cc'
@@ -35,3 +51,4 @@ for f in $files; do
 done
 
 write "build $bin: link $ofiles"
+echo "Wrote ./build.ninja file"
