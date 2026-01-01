5592a53e668c71bc41ce4e6079ab6a365d7b6480
Riley Beckett <rbeckettvt@gmail.com>
2026-01-01 17:17:02 -0500

    fix some errors in build

--- gen.sh
+++ gen.sh
@@ -18,7 +18,7 @@ while [ "$1" ]; do
         --release|-r) release=1 ;;
         --builddir*) builddir="$(echo $1 | cut -d= -f2)" ;;
         --prefix*) PREFIX="$(echo $1 | cut -d= -f2)" ;;
-        --_install) installing=1 ;;
+        --install) installing=1 ;;
 		-*) exit 1 ;;
 	esac
 	shift
@@ -34,33 +34,31 @@ write() {
     echo "$@" >> build.ninja
 }
 
+mkdir -p $builddir
+
 rm -f build.ninja
 
 write "builddir = $builddir"
 write "cflags = $CFLAGS $(pkg-config --cflags $libs 2>/dev/null)"
 write "libs = $(pkg-config --libs $libs 2>/dev/null)"
-write ''
 
 write 'rule cc'
 write '  deps = gcc'
 write '  depfile = $out.d'
 write '  description = CC $out'
 write '  command = gcc -MD -MF $out.d $cflags -c $in -o $out'
-write ''
 
 write 'rule link'
 write '  description = LD $out'
 write '  command = gcc $libs $in -o $out'
-write ''
 
 write 'rule run_install'
 write '  description = Installing Project'
-write "  command = ./gen.sh --_install --prefix=${PREFIX}"
-write ''
+write "  command = ./gen.sh --install --prefix=${PREFIX}"
 
 files="$(find src -name '*.c')"
 sedstr=$(printf 's,^src/\(.*\)\.c$,%s/\\1.o,g' ${builddir})
-ofiles=$(echo $files | sed ${sedstr})
+ofiles=$(echo "$files" | sed ${sedstr})
 
 for f in $files; do
     of=$(echo $f | sed ${sedstr})
@@ -68,8 +66,7 @@ for f in $files; do
 done
 
 write "build install: run_install"
-
-write "build $bin: link $ofiles"
+write "build $bin: link $(echo $ofiles)"
 write "default ${bin}"
 
 echo "Wrote ./build.ninja file"
